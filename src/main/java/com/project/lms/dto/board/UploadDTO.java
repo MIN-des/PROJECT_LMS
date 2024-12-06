3c79c1d5e26c2ac4da0d66c04c9b4659a9240fb2
dkdltl99 <99ekgp@gmail.com>
2024-12-06 12:18:50 +0900

    변다혜 test1

---
+++ src/main/java/com/project/lms/dto/board/UploadDTO.java
@@ -0,0 +1,13 @@
+package com.project.lms.dto.board;
+
+import com.project.lms.entity.Files;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class UploadDTO {
+    private List<Files> files;
+}
+
+
