f736aa25ff70e5846936ef19a7ccbdc5228f7b72
hihihi48 <75200172+hihihi48@users.noreply.github.com>
2024-12-13 16:25:15 +0900

    Delete src directory

--- src/main/java/com/project/lms/constant/RestStatus.java
+++
@@ -1,6 +0,0 @@
-package com.project.lms.constant;
-
-public enum RestStatus {
-    AVAILABLE, // 잔여 O
-    FULL // 잔여 X
-}
--- src/main/java/com/project/lms/exception/OutOfRestNumException.java
+++
@@ -1,8 +0,0 @@
-package com.project.lms.exception;
-
-// 잔여인원 0보다 작은 경우 오류 메시지 발생시키는 클래스
-public class OutOfRestNumException extends RuntimeException {
-    public OutOfRestNumException(String message) {
-        super(message);
-    }
-}
